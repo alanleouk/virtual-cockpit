41627ab07e60bf1c39370f45d923bfbe44840205
Alan Willis <alanleouk@gmail.com>
2022-03-20 08:48:00 +0100

    Stripped back unused view items

--- src/VirtualCockpit/VirtualCockpit.Wpf/SimvarsViewModel.cs
+++ src/VirtualCockpit/VirtualCockpit.Wpf/SimvarsViewModel.cs
@@ -167,39 +167,6 @@ namespace Simvars
         }
         private uint m_iObjectIdRequest = 0;
 
-
-        public string[] aSimvarNames
-        {
-            get { return SimUtils.SimVars.Names; }
-            private set { }
-        }
-        public string sSimvarRequest
-        {
-            get { return m_sSimvarRequest; }
-            set { this.SetProperty(ref m_sSimvarRequest, value); }
-        }
-        private string m_sSimvarRequest = null;
-
-
-        public string[] aUnitNames
-        {
-            get { return SimUtils.Units.Names; }
-            private set { }
-        }
-        public string sUnitRequest
-        {
-            get { return m_sUnitRequest; }
-            set { this.SetProperty(ref m_sUnitRequest, value); }
-        }
-        private string m_sUnitRequest = null;
-
-        public string sSetValue
-        {
-            get { return m_sSetValue; }
-            set { this.SetProperty(ref m_sSetValue, value); }
-        }
-        private string m_sSetValue = null;
-
         public ObservableCollection<SimvarRequest> lSimvarRequests { get; private set; }
         public SimvarRequest oSelectedSimvarRequest
         {
@@ -207,35 +174,6 @@ namespace Simvars
             set { this.SetProperty(ref m_oSelectedSimvarRequest, value); }
         }
         private SimvarRequest m_oSelectedSimvarRequest = null;
-
-        public uint[] aIndices
-        {
-            get { return m_aIndices; }
-            private set { }
-        }
-        private readonly uint[] m_aIndices = new uint[100] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                                                            10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-                                                            20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-                                                            30, 31, 32, 33, 34, 35, 36, 37, 38, 39,
-                                                            40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
-                                                            50, 51, 52, 53, 54, 55, 56, 57, 58, 59,
-                                                            60, 61, 62, 63, 64, 65, 66, 67, 68, 69,
-                                                            70, 71, 72, 73, 74, 75, 76, 77, 78, 79,
-                                                            80, 81, 82, 83, 84, 85, 86, 87, 88, 89,
-                                                            90, 91, 92, 93, 94, 95, 96, 97, 98, 99 };
-        public uint iIndexRequest
-        {
-            get { return m_iIndexRequest; }
-            set { this.SetProperty(ref m_iIndexRequest, value); }
-        }
-        private uint m_iIndexRequest = 0;
-
-        public bool bSaveValues
-        {
-            get { return m_bSaveValues; }
-            set { this.SetProperty(ref m_bSaveValues, value); }
-        }
-        private bool m_bSaveValues = true;
 
         public bool bFSXcompatible
         {
@@ -261,11 +199,6 @@ namespace Simvars
 
 
         public BaseCommand cmdToggleConnect { get; private set; }
-        public BaseCommand cmdAddRequest { get; private set; }
-        public BaseCommand cmdRemoveSelectedRequest { get; private set; }
-        public BaseCommand cmdTrySetValue { get; private set; }
-        public BaseCommand cmdLoadFiles { get; private set; }
-        public BaseCommand cmdSaveFile { get; private set; }
 
         #endregion
 
@@ -284,14 +217,26 @@ namespace Simvars
             lErrorMessages = new ObservableCollection<string>();
 
             cmdToggleConnect = new BaseCommand((p) => { ToggleConnect(); });
-            cmdAddRequest = new BaseCommand((p) => { AddRequest((m_iIndexRequest == 0) ? m_sSimvarRequest : (m_sSimvarRequest + ":" + m_iIndexRequest), sUnitRequest, bIsString); });
-            cmdRemoveSelectedRequest = new BaseCommand((p) => { RemoveSelectedRequest(); });
-            cmdTrySetValue = new BaseCommand((p) => { TrySetValue(); });
-            cmdLoadFiles = new BaseCommand((p) => { LoadFiles(); });
-            cmdSaveFile = new BaseCommand((p) => { SaveFile(false); });
 
             m_oTimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
             m_oTimer.Tick += new EventHandler(OnTick);
+
+            AddRequest("GENERAL ENG THROTTLE LEVER POSITION:1", "percent", false);
+            AddRequest("GENERAL ENG THROTTLE LEVER POSITION:2", "percent", false);
+            AddRequest("FLAPS HANDLE INDEX", "number", false);
+            AddRequest("AILERON POSITION", "position", false);
+            AddRequest("AILERON TRIM PCT", "percent", false);
+            AddRequest("ELEVATOR POSITION", "position", false);
+            AddRequest("ELEVATOR TRIM PCT", "percent", false);
+            AddRequest("RUDDER POSITION", "position", false);
+            AddRequest("RUDDER TRIM", "degrees", false);
+            AddRequest("RUDDER TRIM PCT", "percent", false);
+            AddRequest("BRAKE PARKING POSITION", "Bool", false);
+            AddRequest("GEAR AUX POSITION", "percent", false);
+            AddRequest("SPOILERS ARMED", "Bool", false);
+            AddRequest("SPOILERS HANDLE POSITION", "percent", false);
+            AddRequest("AUTOBRAKES ACTIVE", "Bool", false);
+            AddRequest("AUTO BRAKE SWITCH CB", "number", false);
         }
 
         private void Connect()
@@ -500,15 +445,15 @@ namespace Simvars
             lSimvarRequests.Remove(oSelectedSimvarRequest);
         }
 
-        private void TrySetValue()
+        private void TrySetValue(string value)
         {
             Console.WriteLine("TrySetValue");
 
-            if (m_oSelectedSimvarRequest != null && m_sSetValue != null)
+            if (m_oSelectedSimvarRequest != null && value != null)
             {
                 if (!m_oSelectedSimvarRequest.bIsString) {
                     double dValue = 0.0;
-                    if (double.TryParse(m_sSetValue, NumberStyles.Any, null, out dValue))
+                    if (double.TryParse(value, NumberStyles.Any, null, out dValue))
                     {
                         m_oSimConnect.SetDataOnSimObject(m_oSelectedSimvarRequest.eDef, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_DATA_SET_FLAG.DEFAULT, dValue);
                     }
@@ -517,83 +462,9 @@ namespace Simvars
                 {
                     Struct1 sValueStruct = new Struct1()
                     {
-                        sValue = m_sSetValue
+                        sValue = value
                     };
                     m_oSimConnect.SetDataOnSimObject(m_oSelectedSimvarRequest.eDef, SimConnect.SIMCONNECT_OBJECT_ID_USER, SIMCONNECT_DATA_SET_FLAG.DEFAULT, sValueStruct);
-                }
-            }
-        }
-
-        private void LoadFiles()
-        {
-            Microsoft.Win32.OpenFileDialog oOpenFileDialog = new Microsoft.Win32.OpenFileDialog();
-            oOpenFileDialog.Multiselect = true;
-            oOpenFileDialog.Filter = "Simvars files (*.simvars)|*.simvars";
-            if (oOpenFileDialog.ShowDialog() == true)
-            {
-                foreach (string sFilename in oOpenFileDialog.FileNames)
-                {
-                    LoadFile(sFilename);
-                }
-            }
-        }
-
-        private void LoadFile(string _sFileName)
-        {
-            string[] aLines = System.IO.File.ReadAllLines(_sFileName);
-            for (uint i = 0; i < aLines.Length; ++i)
-            {
-                // Format : Simvar,Unit
-                string[] aSubStrings = aLines[i].Split(',');
-                if (aSubStrings.Length >= 2) // format check
-                {
-                    // values check
-                    string[] aSimvarSubStrings = aSubStrings[0].Split(':'); // extract Simvar name from format Simvar:Index
-                    string sSimvarName = Array.Find(SimUtils.SimVars.Names, s => s == aSimvarSubStrings[0]);
-                    string sUnitName = Array.Find(SimUtils.Units.Names, s => s == aSubStrings[1]);
-                    bool bIsString = aSubStrings.Length > 2 && bool.Parse(aSubStrings[2]);
-                    if (sSimvarName != null && (sUnitName != null || bIsString))
-                    {
-                        AddRequest(aSubStrings[0], sUnitName, bIsString);
-                    }
-                    else
-                    {
-                        if (sSimvarName == null)
-                        {
-                            lErrorMessages.Add("l." + i.ToString() + " Wrong Simvar name : " + aSubStrings[0]);
-                        }
-                        if (sUnitName == null)
-                        {
-                            lErrorMessages.Add("l." + i.ToString() + " Wrong Unit name : " + aSubStrings[1]);
-                        }
-                    }
-                }
-                else
-                {
-                    lErrorMessages.Add("l." + i.ToString() + " Bad input format : " + aLines[i]);
-                    lErrorMessages.Add("l." + i.ToString() + " Must be : SIMVAR,UNIT");
-                }
-            }
-        }
-
-        private void SaveFile(bool _bWriteValues)
-        {
-            Microsoft.Win32.SaveFileDialog oSaveFileDialog = new Microsoft.Win32.SaveFileDialog();
-            oSaveFileDialog.Filter = "Simvars files (*.simvars)|*.simvars";
-            if (oSaveFileDialog.ShowDialog() == true)
-            {
-                using (StreamWriter oStreamWriter = new StreamWriter(oSaveFileDialog.FileName, false))
-                {
-                    foreach (SimvarRequest oSimvarRequest in lSimvarRequests)
-                    {
-                        // Format : Simvar,Unit
-                        string sFormatedLine = oSimvarRequest.sName + "," + oSimvarRequest.sUnits +","+ oSimvarRequest.bIsString;
-                        if (bSaveValues)
-                        {
-                            sFormatedLine += ",  " + oSimvarRequest.dValue.ToString();
-                        }
-                        oStreamWriter.WriteLine(sFormatedLine);
-                    }
                 }
             }
         }
